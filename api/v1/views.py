cc56d9c40708b74002741f5d40ebecbc154a9e61
Arkady Karlkvist <austern1@gmail.com>
2018-12-10 15:42:13 +0100

    FIXED: api/v1/urls and views (Gael)

--- api/v1/views.py
+++ api/v1/views.py
@@ -1,18 +1,17 @@
+from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions
 
 from oauth2_provider.ext.rest_framework import TokenHasScope
-from api.settings import SCOPE_ANAGRAFICA_LETTURA_BASE, SCOPE_ANAGRAFICA_LETTURA_COMPLETA, SCOPE_APPARTENENZE_LETTURA
-
-from api.v1 import serializzatori
 from anagrafica.permessi.applicazioni import PERMESSI_NOMI_DICT
+from api.settings import (SCOPE_ANAGRAFICA_LETTURA_BASE,
+    SCOPE_ANAGRAFICA_LETTURA_COMPLETA, SCOPE_APPARTENENZE_LETTURA)
+from ..v1 import serializzatori
+
 
 # /me/anagrafica/base/
 class MiaAnagraficaBase(APIView):
-    """
-    Una vista che ritorna informazioni sulla persona identificata.
-    """
+    """ Una vista che ritorna informazioni sulla persona identificata. """
     permission_classes = (permissions.IsAuthenticated,
                           TokenHasScope)
     required_scopes = [SCOPE_ANAGRAFICA_LETTURA_BASE]
@@ -41,10 +40,7 @@ class MiaAnagraficaCompleta(APIView):
 
 # /me/appartenenze/attuali/
 class MieAppartenenzeAttuali(APIView):
-    """
-    Una vista che ritorna informazioni sulle appartenenze attuali.
-    """
-
+    """ Una vista che ritorna informazioni sulle appartenenze attuali. """
     required_scopes = [SCOPE_APPARTENENZE_LETTURA]
 
     def get(self, request, format=None):
@@ -55,26 +51,18 @@ class MieAppartenenzeAttuali(APIView):
         return Response(dati)
 
 
-class MiaAppartenenzaComplaeta(APIView):
-
+# /me/appartenenze/completa/
+class MiaAppartenenzaCompleta(APIView):
     """
-        ID utente, - Persona
-
-        nome, - Persona
-
-        cognome, - Persona
-
-        indirizzo mail di contatto - Persona
-
-        rispettiva sede di appartenenza, - Persona
-
-        ID comitato,
-
-        nome comitato,
-
-        estensione del comitato R/P/L/T,
-
-        delega
+    ID utente: Persona\n
+    nome: Persona\n
+    cognome: Persona\n
+    indirizzo mail di contatto: Persona\n
+    rispettiva sede di appartenenza: Persona\n
+    ID comitato\n
+    nome comitato\n
+    estensione del comitato R/P/L/T\n
+    delega\n
     """
     permission_classes = (permissions.IsAuthenticated,
                           TokenHasScope)
@@ -83,7 +71,13 @@ class MiaAppartenenzaComplaeta(APIView):
                        SCOPE_APPARTENENZE_LETTURA]
 
     def get(self, request, format=None):
-        me = request.user.persona
+        user = request.user
+        if user.persona is None or not hasattr(user, 'persona'):
+            return Response({'id_persona': None}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            me = user.persona
+
+        comitati_all = list()
 
         # Persona
         dati = {
@@ -96,38 +90,46 @@ class MiaAppartenenzaComplaeta(APIView):
         if me.email is not None:
             dati['email'] = me.email
 
-        # Comitato
-        deleghe = me.deleghe_attuali()
+        # Deleghe
+        list_deleghe = list()
+        for delega in me.deleghe_attuali().order_by('-creazione'):
+            if not delega.tipo in PERMESSI_NOMI_DICT:
+                continue
 
-        l_deleghe = []
-        for delega in deleghe:
-            d_delega = {
+            oggetto = delega.oggetto
+            sede = oggetto.sede if hasattr(oggetto, 'sede') else oggetto
+
+            list_deleghe.append({
                 'id': delega.id,
                 'tipo': PERMESSI_NOMI_DICT[delega.tipo],
-                'appartenenza': delega.oggetto.nome,
-            }
-            l_deleghe.append(d_delega)
-        dati['deleghe'] = l_deleghe
-
-        # appartenenze
-        appartenenze = me.appartenenze_attuali()
-        l_appartenenza = []
-
-        for appartenenza in appartenenze:
-            comitato = appartenenza.sede
-            l_appartenenza.append({
-                'id': comitato.id,
-                'nome': comitato.nome,
-                'tipo': {
-                    'id': appartenenza.membro,
-                    'descrizione': appartenenza.get_membro_display()
-                },
                 'comitato': {
+                    'id': sede.estensione,
+                    'nome': sede.comitato.nome,
+                    'descrizione': sede.get_estensione_display(),
+                },
+            })
+            comitati_all.append(sede.comitato.nome)
+        dati['deleghe'] = list_deleghe
+
+        # Appartenenze
+        list_appartenenze = list()
+        for appartenenza in me.appartenenze_attuali().order_by('-creazione'):
+            comitato = appartenenza.sede
+            list_appartenenze.append({
+                'id': comitato.id,
+                'descrizione': appartenenza.get_membro_display(),
+                'tipo': appartenenza.membro,
+                'comitato': {
+                    'nome': comitato.nome,
                     'id': comitato.estensione,
                     'descrizione': comitato.get_estensione_display()
                 },
             })
-        dati['appartenenze'] = l_appartenenza
+            comitati_all.append(comitato.nome)
+        dati['appartenenze'] = list_appartenenze
+
+        # Tutti i comitati
+        dati['comitati_all'] = set(comitati_all)
 
         return Response(dati)
 
